8c6d82e0601215a34e3c0d291b7a1dd6bb567721
xm4ddy <59680197+xM4ddy@users.noreply.github.com>
2023-09-29 05:28:55 +0100

    move to using PHP to reduce boilerplate

---
+++ pages/index.php
@@ -0,0 +1,10 @@
+<?php include("../includes/head.php") ?>
+
+<body>
+    <div class="centered">
+        <img id="logo" alt="Triangle With Text \'ESOTERIC\'" src="../assets/triangle.png" />
+        <p>Welcome To <span id="brandname">ESOTERIC</span> Skate</p>
+    </div>
+</body>
+
+</html>
