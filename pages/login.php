f2498c4cb55454da4ef8637dd0ada8f63a92831c
xm4ddy <59680197+xM4ddy@users.noreply.github.com>
2023-11-01 14:33:10 +0000

    small updates

--- pages/login.php
+++ pages/login.php
@@ -1,8 +1,16 @@
-<?php readfile("../includes/boilerplate.html"); ?>
+<?php session_start(); readfile("../includes/boilerplate.html"); ?>
 
 <div class="centered">
     <div class="register">
+        <pre>Username: At Least 5 Characters Long,<br />AlphaNumeric Only!<br />Password: At Least 8 Characters Long,<br /> AlphaNumeric + At Least 1 <br />Upercase and 1 Number</pre>
+    </div>
+    <div class="register">
         <form action="../includes/doRegister.php" method="post">
+            <?php
+                if (isset($_SESSION['rError'])) {
+                    echo ("<p class=\"error\">" . $_SESSION['rError'] . "</p>");
+                } 
+            ?>
             <label for="ruser"><b>Username</b></label>
             <input type="text" id="ruser" name="ruser" />
             <br />
@@ -12,25 +20,22 @@
             <label for="rpass"><b>Password</b></label>
             <input type="password" id="rpass" name="rpass" />
             <br />
-            <?php session_start();
-            if (isset($_SESSION['rError'])) {
-                echo ("<p class=\"error\">" . $_SESSION['rError'] . "</p>");
-            } ?>
             <button type="submit">Register</button>
         </form>
     </div>
     <div class="login">
         <form action="../includes/doLogin.php" method="post">
+            <?php
+                if (isset($_SESSION['lError'])) {
+                    echo ("<p class=\"error\">" . $_SESSION['lError'] . "</p>");
+                } 
+            ?>
             <label for="luser"><b>Username</b></label>
             <input type="text" id="luser" name="luser" />
             <br />
             <label for="lpass"><b>Password</b></label>
             <input type="password" id="lpass" name="lpass" />
             <br />
-            <?php
-            if (isset($_SESSION['lError'])) {
-                echo ("<p class=\"error\">" . $_SESSION['lError'] . "</p>");
-            } ?>
             <button type="submit">Login</button>
         </form>
     </div>
